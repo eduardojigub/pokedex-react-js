f62bf5f24ea98ebc23a8abd7e42c9a5515e19184
eduardojigub <eduardobrunoit@gmail.com>
2022-09-04 17:23:55 -0300

    creating initial styles

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -19,15 +19,15 @@ export default function PokemonCard() {
         <Typography gutterBottom variant="h5" component="div">
           Lizard
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
-        </Typography>
+        </Typography> */}
       </CardContent>
-      <CardActions>
+      {/* <CardActions>
         <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import Navbar from '../components/Navbar/index'
-import PokemonCard from '../components/PokemonCard/index'
+import React from "react";
+import Navbar from "../components/Navbar/index";
+import PokemonCard from "../components/PokemonCard/index";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 export const Home = () => {
-    return (
-        <div><Navbar />
-        <PokemonCard />
-        </div>
-
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Container maxWidth='false'>
+        <Grid container>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default Home;
