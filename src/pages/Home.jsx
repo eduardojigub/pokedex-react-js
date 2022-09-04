be63fa160cdabadd76c71d018676e1551e281af0
eduardojigub <eduardobrunoit@gmail.com>
2022-09-04 17:16:37 -0300

    cleaning react files and adding MUI styles and components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar/index'
+import PokemonCard from '../components/PokemonCard/index'
+
+export const Home = () => {
+    return (
+        <div><Navbar />
+        <PokemonCard />
+        </div>
+
+    )
+}
+
+export default Home;
