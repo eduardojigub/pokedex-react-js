bcfd2c7668ff9f678efcdb8fde74cb554dd13ed6
eduardojigub <eduardobrunoit@gmail.com>
2022-09-06 16:17:12 -0300

    fetching poke api using axios

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -1,23 +1,21 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
-export default function PokemonCard() {
+export default function PokemonCard({ name }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
+        component='img'
+        height='140'
+        image='/static/images/cards/contemplative-reptile.jpg'
+        alt='green iguana'
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        <Typography gutterBottom variant='h5' component='div'>
+          {name}
         </Typography>
         {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/index";
 import PokemonCard from "../components/PokemonCard/index";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import axios from "axios";
 
 export const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+      .then((res) => setPokemons(res.data.results))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <Navbar />
       <Container maxWidth='false'>
         <Grid container>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
+          {pokemons.map((pokemon, key) => (
+            <Grid item xs={3} key={key}>
+              <PokemonCard name={pokemon.name} />
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </div>
