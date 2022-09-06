7174051e0aee20b40277221f67be5b373efc3692
eduardojigub <eduardobrunoit@gmail.com>
2022-09-06 17:10:17 -0300

    adding skeletons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import PokemonCard from "../components/PokemonCard/index";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
+import { Skeletons } from "../components/Skeletons";
 
 export const Home = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -14,7 +15,7 @@ export const Home = () => {
 
   const getPokemons = () => {
     var endpoints = [];
-    for (var i = 1; i < 50; i++) {
+    for (var i = 1; i < 151; i++) {
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
     }
     console.log(endpoints);
@@ -48,15 +49,19 @@ export const Home = () => {
       <Navbar pokemonFilter={pokemonFilter} />
       <Container maxWidth='false'>
         <Grid container spacing={3}>
-          {pokemons.map((pokemon, key) => (
-            <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
-              <PokemonCard
-                name={pokemon.data.name}
-                image={pokemon.data.sprites.front_default}
-                types={pokemon.data.types}
-              />
-            </Grid>
-          ))}
+          {pokemons.length === 0 ? (
+            <Skeletons />
+          ) : (
+            pokemons.map((pokemon, key) => (
+              <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
+                <PokemonCard
+                  name={pokemon.data.name}
+                  image={pokemon.data.sprites.front_default}
+                  types={pokemon.data.types}
+                />
+              </Grid>
+            ))
+          )}
         </Grid>
       </Container>
     </div>
