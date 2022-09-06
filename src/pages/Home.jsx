0591006d714299bd2d644af30cc1cb5aa0c42577
eduardojigub <eduardobrunoit@gmail.com>
2022-09-06 17:18:29 -0300

    adding toLowercase on search Input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ export const Home = () => {
       getPokemons();
     }
     for (var i in pokemons) {
-      if (pokemons[i].data.name.includes(name)) {
+      if (pokemons[i].data.name.includes(name.toLowerCase())) {
         filteredPokemons.push(pokemons[i]);
       }
     }
